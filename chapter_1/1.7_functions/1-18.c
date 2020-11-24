8674b065c24f4707cc4221039e4b485eebb4389c
Adam Solloway <asolloway@worcester.edu>
2020-11-24 13:31:50 -0500

    white_check

--- chapter_1/1.7_functions/1-18.c
+++ chapter_1/1.7_functions/1-18.c
@@ -5,7 +5,7 @@
 
 int get_line(char line[], int maxline);
 void trim_line(char line[], int len);
-void copy(char to[], char from[], int len);
+int white_check(char c);
 
 // Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines.
 int main(int argc, char const *argv[]) {
@@ -14,30 +14,20 @@ int main(int argc, char const *argv[]) {
     int len;
 
     while ((len = get_line(input, MAXLINE)) > 0) {
-        // trim_line(input, len);
-        // copy(line, input, len);
         trim_line(input, len);
         printf("%s\n", input);
     }
     return 0;
 }
 
-void copy(char to[], char from[], int len) {
-    for (int i = 0; i < len; i++) {
-        to[i] = from[i];
-    }
-}
-
 void trim_line(char line[], int len) {
-    int n = 0;                      // position of char
+    int n = 0;                       // position of char
     for (int i = 0; i <= len; i++) { // get index of last char
         if (line[i] != '-' && line[i] != '\n' && line[i] != '\0') {
             n = i;
         }
     }
-    for (int i = n+1; i <= len; i++) { // erase everything after last char (trailing whitespace)
-        line[i] = '\0';
-    }
+    line[n + 1] = '\0';
 }
 
 int get_line(char s[], int lim) {
@@ -55,4 +45,15 @@ int get_line(char s[], int lim) {
     }
 
     return i;
+}
+
+int white_check(char s) {
+    const int len = 3;
+    char white[len] = {' ', '\n', '\t'};
+    for (int i = 0; i < len; i++) {
+        if (s == white[i]) {
+            return 1;
+        }
+    }
+    return 0;
 }
