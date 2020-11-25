8a64fcb15e61194c045d2a68304cafba0a13ae24
Adam Solloway <asolloway@worcester.edu>
2020-11-25 17:30:10 -0500

    trims but keeps empty

--- chapter_1/1.7_functions/1-18.c
+++ chapter_1/1.7_functions/1-18.c
@@ -4,34 +4,38 @@
 #define MAXLINE 100
 
 int get_line(char line[], int maxline);
-void trim_line(char line[], int len);
+void trim_line(char input[], int len);
 int white_check(char c);
+void copy(char to[], char from[]);
 
 // Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines.
 int main(int argc, char const *argv[]) {
     char input[MAXLINE];
     char line[MAXLINE];
     int len;
+    int empty = 1;
 
     while ((len = get_line(input, MAXLINE)) > 0) {
-        trim_line(input, len);
-        printf("%s\n", input);
+        copy(line, input);
+        trim_line(line, len);
+        printf("%s", line);
     }
     return 0;
 }
 
-void trim_line(char line[], int len) {
-    int n = 0;                       // position of char
-    for (int i = 0; i <= len; i++) { // get index of last char
-        if (line[i] != '-' && line[i] != '\n' && line[i] != '\0') {
+void trim_line(char input[], int len) {
+    int n = 0;                      // position of char
+    for (int i = 0; i < len; i++) { // get index of last char
+        if (input[i] != '-' && input[i] != '\n' && input[i] != '\0') {
             n = i;
         }
     }
-    line[n + 1] = '\0';
+    input[n + 1] = '\0';
 }
 
 int get_line(char s[], int lim) {
     int c, i;
+
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
         if (c == ' ') {
             s[i] = '-';
@@ -40,16 +44,25 @@ int get_line(char s[], int lim) {
         }
     }
     if (c == '\n') {
-        s[i] = '\0';
+        s[i] = c;
         i++;
     }
-
+    s[i] = '\0';
     return i;
 }
 
+void copy(char to[], char from[]) {
+    int i = 0;
+
+    while (from[i]) {
+        to[i] = from[i];
+        i++;
+    }
+}
+
 int white_check(char s) {
-    const int len = 3;
-    char white[len] = {' ', '\n', '\t'};
+    const int len = 4;
+    char white[len] = {'-', ' ', '\n', '\t'};
     for (int i = 0; i < len; i++) {
         if (s == white[i]) {
             return 1;
