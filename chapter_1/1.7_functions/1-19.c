f3645962eac8833ac28d2a6434e9646e4501434f
Adam Solloway <asolloway@worcester.edu>
2020-12-19 01:10:51 -0500

    String reverse

---
+++ chapter_1/1.7_functions/1-19.c
@@ -0,0 +1,39 @@
+// 2020-11-27
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int get_line(char line[], int maxline);
+void reverse(char from[], char to[], int len);
+
+// Write a function reverse(s) that reverses the character string s.
+// Use it to write a program that reverses its input a line at a time
+int main(int argc, char const *argv[]) {
+    int len;
+    char line[MAXLINE];
+    char rev[MAXLINE];
+
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        reverse(line, rev, len);
+    }
+    printf("%s", rev);
+    return 0;
+}
+
+int get_line(char line[], int maxline) {
+    int c, i;
+
+    for (i = 0; i < maxline && (c = getchar()) != EOF; i++) {
+        line[i] = c;
+    }
+    return i;
+}
+
+void reverse(char from[], char to[], int len) {
+    int count = 0;
+
+    for (int i = len - 1; i >= 0; i--) {
+        to[count] = from[i];
+        count++;
+    }
+}
