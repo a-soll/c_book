9904e3888ce45084d469ffbbb01e4c5ddcb267b5
Adam Solloway <asolloway@worcester.edu>
2020-12-25 14:10:49 -0500

    Only exercises.

---
+++ exercises/1-20.c
@@ -0,0 +1,7 @@
+// 2020-12-19
+
+#include <stdio.h>
+
+// Write a program called tedab that replaces tabs in the input with the proper
+// number of blanks to create space to the next tab stop assume a fixed set of tab stops,
+// say every n columns. Should n be a variable or symbolic parameter?
